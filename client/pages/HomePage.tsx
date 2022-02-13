e4d6dca9026b48db766a7241cb7db6980f77f50e
Olav Hartwedt Larsen <ohalar@gmail.com>
2022-02-13 18:36:30 +0100

    Added some content and navigation on a couple pages

--- client/pages/HomePage.tsx
+++ client/pages/HomePage.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 export function HomePage() {
-  return <h1>Start new Quiz</h1>;
+  return (
+    <>
+      <h1>Start new Quiz</h1>
+      <Link to={"/question"}>
+        <button>New question</button>
+      </Link>
+    </>
+  );
 }
--- client/pages/ScorePage.tsx
+++ client/pages/ScorePage.tsx
@@ -1,5 +1,18 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 export function ScorePage() {
-  return <h1>Score</h1>;
+  return (
+    <>
+      <h1>Score</h1>
+      <ul>
+        <li>
+          <Link to={"/question"}>New Question</Link>
+        </li>
+        <li>
+          <Link to={"/score"}>See score</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
