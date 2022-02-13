67bd50b8f858ef431631bdee50edb8e1b730f230
Olav Hartwedt Larsen <ohalar@gmail.com>
2022-02-13 17:38:49 +0100

    Added middleware to server.ts

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,15 @@ import express = require("express");
 import {AddressInfo} from 'net';
 import {QuizApp} from "./routes/quizApp";
 import bodyParser from "body-parser";
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use("/api/quiz", QuizApp)
+dotenv.config();
 app.use(bodyParser.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use("/api/quiz", QuizApp)
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started on http://localhost:${(server.address() as AddressInfo).port}`);
