8bd8f0d5f653503eef26dcea08df137752cf118c
Olav Hartwedt Larsen <ohalar@gmail.com>
2022-02-13 18:42:11 +0100

    Connecting client and server

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import { QuizApp } from "./routes/quizApp";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import * as path from "path";
 
 const app = express();
 
@@ -11,6 +12,15 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use("/api/quiz", QuizApp);
+
+app.use(express.static(path.resolve("..", "client", "dist")));
+
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(path.resolve("..", "client", "dist", "index.html"));
+  }
+  next();
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(
