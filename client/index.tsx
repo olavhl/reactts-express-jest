58d2e99a241b5b36e73557a14876208a00f005fd
Olav Hartwedt Larsen <ohalar@gmail.com>
2022-02-13 18:24:45 +0100

    Formatted pages

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import {Application} from "./Application";
+import { Application } from "./Application";
 
-
-ReactDOM.render(<Application/>, document.getElementById("app"));
+ReactDOM.render(<Application />, document.getElementById("app"));
